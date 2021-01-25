7eb54251e6ec07b4f710f95a9adcd91584749926
Nigora <61627731+NigoraFayzullaeva@users.noreply.github.com>
2021-01-25 14:52:01 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
+const video = document.querySelector('video');
+const progressRange = document.querySelector('.progress-range');
+const progressBar = document.querySelector('.progress-bar');
+const playBtn = document.getElementById('play-btn');
+const volumeIcon = document.getElementById('volume-icon');
+const volumeRange = document.querySelector('.volume-range');
+const volumeBar = document.querySelector('.volume-bar');
+const currentTime = document.querySelector('.time-elapsed');
+const duration = document.querySelector('.time-duration');
+const fullscreenBtn = document.querySelector('.fullscreen');
+
 
 
 // Play & Pause ----------------------------------- //
 
+function showPlayIcon() {
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'Play');
+}
 
+function togglePlay() {
+    if (video.paused){
+        video.play();
+        playBtn.classList.replace('fa-play', 'fa-pause');
+        playBtn.setAttribute('title', 'Pause');
+    }else{
+        video.pause();
+        showPlayIcon();
+    }
+}
+
+// On video end, show play button icon
+video.addEventListener('ended', showPlayIcon);
 
 // Progress Bar ---------------------------------- //
 
@@ -18,4 +46,7 @@
 
 // Fullscreen ------------------------------- //
 
+// Event Listeners
+playBtn.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay);
 
